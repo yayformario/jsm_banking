d685f1102f3186aedbd71133ae9e29fd742cbc24
Mario Huerta <yayformario@gmail.com>
2024-10-12 20:15:17 -0400

    main menu and sidebar progress

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,35 @@
+import MobileNav from "@/components//ui/MobileNav";
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <main>
-        SIDEBAR
-        {children}
-    </main>
-  )
 
+
+  const loggedIn = { 
+    firstName: 'Mario', 
+    lastName: 'Huerta'
+  };
+  
+  
+  return (
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src = "/icons/logo.svg" width={30} height={30} alt="logo"
+            />
+
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = {FirstName: 'Mario' };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            
+            <div className="home-content">
+
+                <header className="home-header">
+                   <HeaderBox
+                   type="greeting"
+                   title="Welcome"
+                   user={loggedIn?.FirstName || 'Guest'}
+                   subtext="Access and manage your accounts or transactions"
+                   />
+
+                   <TotalBalanceBox 
+                   accounts={[]}
+                   totalBanks={1}
+                   totalCurrentBalance = {1250.35}
+                   />
+
+                </header>
+
+            </div>
+        
+        </section>
     )
 }
 
