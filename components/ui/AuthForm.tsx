deacb3f0bf220ad75fbb439f3fa84aca81ffdebe
Pixiestickz <23437228+Pixiestickz@users.noreply.github.com>
2024-12-11 12:39:01 -0500

    implemented PlaidLink and Dwolla

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ( {type}: {type: string}) => {
@@ -44,12 +45,26 @@ const AuthForm = ( {type}: {type: string}) => {
     const onSubmit = async (data: z.infer<typeof formSchema>) =>{
         setIsLoading(true)
         try {
-
             // Sign up with Appwrite & create plaid link token
             if (type === 'sign-up') {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dob!,
+                    ssn: data.ssn!,
+
+                    email: data.email,
+                    password: data.password,
+                }
+
+                const newUser = await signUp(userData);
                 setUser(newUser);
             }
+
 
             if (type === 'sign-in') {
                 const response = await signIn({
@@ -107,7 +122,7 @@ const AuthForm = ( {type}: {type: string}) => {
 
         {user ? (
             <div className='flex flex-col gap-4'>
-                {/* Plaid link to link bank account*/}
+                <PlaidLink user = {user} variant = "primary"/>
             </div>
         ): (
             <>
